963531e6c41841e37c49bbc6f8e0c262592dbb3c
Shaurya Gupta <shauryakanha2@gmail.com>
2026-02-27 07:39:23 +0530

    Atlas Connection

--- src/confiig/mongodb-connect.js
+++ src/confiig/mongodb-connect.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-mongoose.connect("mongodb://127.0.0.1:27017/theHardwareHub")
+mongoose.connect("mongodb+srv://HardwareHub:qwert@hardwarehub.pclj3wl.mongodb.net/?appName=HardwareHub")
 
 
 .then(function(){
